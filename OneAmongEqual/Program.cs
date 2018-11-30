59ea805863d0991a27917438a07faaa1b2e181c0
yuriy.maturin <yuriy.maturin@gmail.com>
2018-12-01 01:44:16 +0200

    New

--- OneAmongEqual/Program.cs
+++ OneAmongEqual/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,63 @@ namespace OneAmongEqual
 {
     class Program
     {
+        static bool FileEquals(string path1, string path2)
+        {
+            byte[] file1 = File.ReadAllBytes(path1);
+            byte[] file2 = File.ReadAllBytes(path2);
+            if (file1.Length == file2.Length)
+            {
+                for (int i = 0; i < file1.Length; i++)
+                {
+                    if (file1[i] != file2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        static void Method0(string PATH, int j)
+        {
+            FileInfo[] files = new DirectoryInfo(PATH).GetFiles().ToArray();
+            if (j<files.Length -1)
+            {
+                string path1 = PATH + @"\" + files[j].Name;
+                for (int i = j+1; i < files.Length - 1; i++)
+                {
+                    string path2 = PATH + @"\" + files[i].Name;
+                    if (FileEquals(path1, path2))
+                    {
+                        files[i].Delete();
+                    }
+                }
+                
+            }
+        }
+        static void MainMethod(string PATH)
+        {
+            FileInfo[] files = new DirectoryInfo(PATH).GetFiles().ToArray();
+            int v = files.Length;
+            if (v > 1)
+            {
+                int j = 0;
+
+                do
+                {
+                    Method0(PATH, j);
+                    j++;
+                }
+                while (j < new DirectoryInfo(PATH).GetFiles().Length);
+
+            }
+        }
         static void Main(string[] args)
         {
+            string PATH = @"D:\L";
+            // write the path
+            MainMethod(PATH);
+            Console.ReadKey();
         }
     }
 }
